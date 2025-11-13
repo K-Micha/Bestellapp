7a61a7ecb4204e9b8e69880e142b8490034e3c4e
K-Micha <Micha.kuehn91@gmail.com>
2025-11-13 14:53:17 +0100

    Basic

---
+++ scripts/header_footer.js
@@ -0,0 +1,12 @@
+fetch("html/header.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header_content").innerHTML = data;
+
+//fetch("footer.html")
+         //   .then((response) => response.text())
+          //  .then((data) => {
+           //     document.getElementById("footer_content").innerHTML = data;
+            });
+
+    
