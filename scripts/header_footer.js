b235503b1f4a96d377bde5604b5c8cc73f15f70e
K-Micha <Micha.kuehn91@gmail.com>
2025-11-16 15:05:29 +0100

    impressum

--- scripts/header_footer.js
+++ scripts/header_footer.js
@@ -1,9 +1,9 @@
-fetch("html/header.html")
+fetch("/html/header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header_content").innerHTML = data;
 
-    fetch("html/footer.html")
+    fetch("/html/footer.html")
       .then((response) => response.text())
      .then((data) => {
          document.getElementById("footer_content").innerHTML = data;
